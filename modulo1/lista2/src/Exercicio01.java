2798813e45fe6d64f27627f0d79411dda315d466
Marcklen <marcklen@icloud.com>
2023-02-06 18:33:44 -0300

    feat: adicionado o exercicio 01 da lista 02 - calculando desconto de produtos num laço

---
+++ modulo1/lista2/src/Exercicio01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o nome de um produto: ");
+        String produto = scanner.nextLine().toLowerCase();
+        System.out.print("Digite o preço do produto: ");
+        double preco = scanner.nextDouble();
+        System.out.print("Promoção: " + produto + "\n-------------------------\n");
+        float desconto = 0.05f;
+        for (int i = 1; i <= 10; i++) {
+//            System.out.println(i + " x R$ " + (preco - (preco * desconto)) + " = R$ " + (preco - (preco * desconto)) * i );
+            System.out.printf(i + " x R$ %.2f = R$ %.2f\n", (preco - (preco * desconto)), (preco - (preco * desconto)) * i);
+//            System.out.println();
+            desconto += 0.05f;
+        }
+    }
+}
