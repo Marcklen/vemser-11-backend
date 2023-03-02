9a4dd3602ca8b72104672ceb4f030ed37349b587
Marcklen <marcklen@icloud.com>
2023-03-02 17:58:43 -0300

    finalizando task3 endereco - validacoes e response entity

--- modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/controller/EnderecoController.java
+++ modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/controller/EnderecoController.java
@@ -2,12 +2,17 @@ package br.com.dbc.vemser.pessoaapi.controller;
 
 import br.com.dbc.vemser.pessoaapi.entity.Endereco;
 import br.com.dbc.vemser.pessoaapi.service.EnderecoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/endereco") // http://localhost:8080/endereco
+@Validated
 public class EnderecoController {
 
     private final EnderecoService enderecoService;
@@ -17,34 +22,37 @@ public class EnderecoController {
     }
 
     @GetMapping
-    public List<Endereco> listarTodos() {
-        return enderecoService.listarTodos();
+    public ResponseEntity<List<Endereco>> listarTodos() {
+        return new ResponseEntity<>(enderecoService.listarTodos(), HttpStatus.OK);
     }
 
     @GetMapping("/{idPessoa}/pessoa")
-    public List<Endereco> listarEnderecosPorPessoa(@PathVariable("idPessoa") Integer idPessoa) throws Exception {
-        return enderecoService.listarEnderecosPorIdPessoa(idPessoa);
+    public ResponseEntity<List<Endereco>> listarEnderecosPorPessoa(@PathVariable("idPessoa") Integer idPessoa)
+            throws Exception {
+        return new ResponseEntity<>(enderecoService.listarEnderecosPorIdPessoa(idPessoa), HttpStatus.OK);
     }
 
     @GetMapping("/{idEndereco}")
-    public List<Endereco> buscarEnderecoPorId(@PathVariable("idEndereco") Integer idEndereco) throws Exception {
-        return enderecoService.buscarEnderecoPorId(idEndereco);
+    public ResponseEntity<List<Endereco>> buscarEnderecoPorId(@PathVariable("idEndereco") Integer idEndereco)
+            throws Exception {
+        return new ResponseEntity<>(enderecoService.buscarEnderecoPorId(idEndereco), HttpStatus.OK);
     }
 
     @PostMapping("/{idPessoa}")
-    public Endereco create(@PathVariable("idPessoa") Integer idPessoa,
-                           @RequestBody Endereco endereco) throws Exception {
-        return enderecoService.criarEndereco(idPessoa, endereco);
+    public ResponseEntity<Endereco> create(@PathVariable("idPessoa") Integer idPessoa,
+                                           @Valid @RequestBody Endereco endereco) throws Exception {
+        return new ResponseEntity<>(enderecoService.criarEndereco(idPessoa, endereco), HttpStatus.CREATED);
     }
 
     @PutMapping("/{idEndereco}")
-    public Endereco atualizarEndereco(@PathVariable("idEndereco") Integer idEndereco,
-                                      @RequestBody Endereco enderecoAtualizar) throws Exception {
-        return enderecoService.atualizarEndereco(idEndereco, enderecoAtualizar);
+    public ResponseEntity<Endereco> atualizarEndereco(@PathVariable("idEndereco") Integer idEndereco,
+                                                      @Valid @RequestBody Endereco enderecoAtualizar) throws Exception {
+        return new ResponseEntity<>(enderecoService.atualizarEndereco(idEndereco, enderecoAtualizar), HttpStatus.OK);
     }
 
     @DeleteMapping("/{idEndereco}")
-    public void deletarEndereco(@PathVariable("idEndereco") Integer idEndereco) throws Exception {
+    public ResponseEntity<Void> deletarEndereco(@PathVariable("idEndereco") Integer idEndereco) throws Exception {
         enderecoService.deletarEndereco(idEndereco);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/entity/Endereco.java
+++ modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/entity/Endereco.java
@@ -1,15 +1,31 @@
 package br.com.dbc.vemser.pessoaapi.entity;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 public class Endereco {
     private Integer idEndereco;
     private Integer idPessoa;
+    @NotNull(message = "Tipo de endereço não pode ser nulo")
     private TipoEndereco tipo;
+    @NotEmpty(message = "Logradouro não pode ser vazio")
+    @Length(max = 250, message = "Logradouro não pode ter mais de 250 caracteres")
     private String logradouro;
+    @NotNull(message = "Número não pode ser nulo")
     private Integer numero;
     private String complemento;
+    @NotBlank(message = "CEP não pode ser vazio")
+    @Length(min = 8, max = 8, message = "CEP não pode ter mais de 8 caracteres")
     private String cep;
+    @NotBlank(message = "CEP não pode ser vazio")
+    @Length(max = 250, message = "Cidade não pode ter mais de 250 caracteres")
     private String cidade;
+    @NotNull(message = "Estado não pode ser nulo")
     private String estado;
+    @NotNull(message = "País não pode ser nulo")
     private String pais;
 
     public Endereco(){}
