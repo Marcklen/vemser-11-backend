df9802570f849e2d66935af08679d885feb72479
Marcklen <marcklen@icloud.com>
2023-03-01 23:03:25 -0300

    metodo atualizar com validacoes - ok

--- modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/service/EnderecoService.java
+++ modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/service/EnderecoService.java
@@ -1,8 +1,9 @@
 package br.com.dbc.vemser.pessoaapi.service;
 
 import br.com.dbc.vemser.pessoaapi.entity.Endereco;
-import br.com.dbc.vemser.pessoaapi.entity.TipoEndereco;
 import br.com.dbc.vemser.pessoaapi.repository.EnderecoRepository;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -33,12 +34,15 @@ public class EnderecoService {
     public Endereco criarEndereco(Integer idPessoa, Endereco endereco) throws Exception {
         getEndereco(idPessoa);
         endereco.setIdPessoa(idPessoa);
+        validarEndereco(endereco);
         return enderecoRepository.criarEndereco(endereco);
     }
 
     public Endereco atualizarEndereco(Integer idEndereco, Endereco enderecoAtualizar) throws Exception {
         Endereco enderecoRecuperado = getEndereco(idEndereco);
-//        enderecoRecuperado.setIdPessoa(enderecoAtualizar.getIdPessoa()); // não pode atualizar o idPessoa essa linha é necessária ?
+        validarEndereco(enderecoAtualizar);
+
+        enderecoRecuperado.setIdEndereco(idEndereco);
         enderecoRecuperado.setTipo(enderecoAtualizar.getTipo());
         enderecoRecuperado.setLogradouro(enderecoAtualizar.getLogradouro());
         enderecoRecuperado.setNumero(enderecoAtualizar.getNumero());
@@ -47,8 +51,10 @@ public class EnderecoService {
         enderecoRecuperado.setCidade(enderecoAtualizar.getCidade());
         enderecoRecuperado.setEstado(enderecoAtualizar.getEstado());
         enderecoRecuperado.setPais(enderecoAtualizar.getPais());
+
         return enderecoRecuperado;
     }
+
 
     public void deletarEndereco(Integer idEndereco) throws Exception {
         Endereco enderecoRecuperado = getEndereco(idEndereco);
@@ -63,5 +69,39 @@ public class EnderecoService {
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Endereço não encontrado!"));
         return enderecoRecuperado;
+    }
+
+    private boolean validarEndereco(Endereco endereco) throws Exception {
+        if (StringUtils.isBlank(endereco.getTipo().toString()) ||
+                ObjectUtils.isEmpty(endereco.getTipo())) {
+            throw new Exception("O tipo do endereço não pode ser nulo!");
+        }
+        if (StringUtils.isBlank(endereco.getLogradouro())) {
+            throw new Exception("O logradouro do endereço não pode ser nulo!");
+        }
+        if (ObjectUtils.isEmpty(endereco.getNumero())) {
+            throw new Exception("O número do endereço não pode ser nulo!");
+        }
+        if (StringUtils.length(endereco.getNumero().toString())  > 5) {
+            throw new Exception("O número do endereço não pode ser maior que 5 caracteres!");
+        }
+        if (StringUtils.isBlank(endereco.getCep()) ||
+                StringUtils.length(endereco.getCep())  < 8 &&
+                        StringUtils.length(endereco.getCep())  > 10) {
+            throw new Exception("O CEP do endereço não pode ser nulo, nem menor que 8 caracteres " +
+                    "e nem maior que 10 caracteres!");
+        }
+        if (StringUtils.isBlank(endereco.getCidade())) {
+            throw new Exception("A cidade do endereço não pode ser nula!");
+        }
+        if (StringUtils.isBlank(endereco.getEstado()) ||
+                StringUtils.length(endereco.getEstado())  != 2) {
+            throw new Exception("O estado do endereço não pode ser nulo e nem ter mais que 2 caracteres!" +
+                    "Exemplo: CE, SP, RS");
+        }
+        if (StringUtils.isBlank(endereco.getPais())) {
+            throw new Exception("O país do endereço não pode ser nulo!");
+        }
+        return true;
     }
 }
