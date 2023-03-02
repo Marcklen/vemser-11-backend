11184b4143a1e296435d065727a2685866079c75
Marcklen <marcklen@icloud.com>
2023-03-02 16:21:10 -0300

    Exercicio01 - adicionado validations e controle de excessoes na entidade pessoa e pessoaController

--- modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/controller/PessoaController.java
+++ modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/controller/PessoaController.java
@@ -3,12 +3,17 @@ package br.com.dbc.vemser.pessoaapi.controller;
 import br.com.dbc.vemser.pessoaapi.entity.Pessoa;
 import br.com.dbc.vemser.pessoaapi.service.PessoaService;
 import br.com.dbc.vemser.pessoaapi.service.PropertieReader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/pessoa") // http://localhost:8080/pessoa
+@Validated
 public class PessoaController {
 
     private final PessoaService pessoaService;
@@ -31,33 +36,34 @@ public class PessoaController {
     }
 
     @GetMapping // GET localhost:8080/pessoa
-    public List<Pessoa> list() {
-        return pessoaService.list();
+    public ResponseEntity<List<Pessoa>> list() {
+        return new ResponseEntity<>(pessoaService.list(), HttpStatus.OK);
     }
 
     @GetMapping("/byname") // GET localhost:8080/pessoa/byname?nome=Rafa
-    public List<Pessoa> listByName(@RequestParam("nome") String nome) {
-        return pessoaService.listByName(nome);
+    public ResponseEntity<List<Pessoa>> listByName(@RequestParam("nome") String nome) {
+        return new ResponseEntity<>(pessoaService.listByName(nome), HttpStatus.OK);
     }
 
     @GetMapping("/{idPessoa}") // GET localhost:8080/pessoa/1
-    public Pessoa findById(@PathVariable("idPessoa") Integer id) {
-        return pessoaService.findById(id);
+    public ResponseEntity<Pessoa> findById(@PathVariable("idPessoa") Integer id) {
+        return new ResponseEntity<>(pessoaService.findById(id), HttpStatus.OK);
     }
 
     @PostMapping
-    public Pessoa create(@RequestBody Pessoa pessoa) throws Exception{
-        return pessoaService.create(pessoa);
+    public ResponseEntity<Pessoa> create(@Valid @RequestBody Pessoa pessoa) throws Exception{
+        return new ResponseEntity<>(pessoaService.create(pessoa), HttpStatus.CREATED);
     }
 
     @PutMapping("/{idPessoa}")
-    public Pessoa update(@PathVariable("idPessoa") Integer id,
-                         @RequestBody Pessoa pessoaAtualizar) throws Exception {
-        return pessoaService.update(id, pessoaAtualizar);
+    public ResponseEntity<Pessoa> update(@PathVariable("idPessoa") Integer id,
+                         @Valid @RequestBody Pessoa pessoaAtualizar) throws Exception {
+        return new ResponseEntity<>(pessoaService.update(id, pessoaAtualizar), HttpStatus.OK);
     }
 
     @DeleteMapping("/{idPessoa}")
-    public void delete(@PathVariable("idPessoa") Integer id) throws Exception {
+    public ResponseEntity<Void> delete(@PathVariable("idPessoa") Integer id) throws Exception {
         pessoaService.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
--- modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/entity/Pessoa.java
+++ modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/entity/Pessoa.java
@@ -1,11 +1,20 @@
 package br.com.dbc.vemser.pessoaapi.entity;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 public class Pessoa {
     private Integer idPessoa;
+    @NotBlank(message = "O Nome não pode ser vazio nem nulo")
     private String nome;
+    @NotNull(message = "A Data de Nascimento não pode ser vazia nem nula")
+    @PastOrPresent(message = "A Data de Nascimento não pode ser futura")
     private LocalDate dataNascimento;
+
+    @NotBlank(message = "O CPF não pode ser vazio nem nulo")
+    @Length(min = 11, max = 11, message = "O CPF deve conter exatamente 11 caracteres")
     private String cpf;
 
     public Pessoa() {
