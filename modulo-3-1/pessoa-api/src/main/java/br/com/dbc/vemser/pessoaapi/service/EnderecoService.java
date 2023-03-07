2f4f492d8080b6e08da02541c325e855d2eb7c34
Marcklen <marcklen@icloud.com>
2023-03-06 21:41:14 -0300

    task6 - enviando emails quando se cadastra, altera ou exclui enderecos

--- modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/service/EnderecoService.java
+++ modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/service/EnderecoService.java
@@ -2,28 +2,27 @@ package br.com.dbc.vemser.pessoaapi.service;
 
 import br.com.dbc.vemser.pessoaapi.dto.EnderecoCreateDTO;
 import br.com.dbc.vemser.pessoaapi.dto.EnderecoDTO;
+import br.com.dbc.vemser.pessoaapi.dto.PessoaDTO;
 import br.com.dbc.vemser.pessoaapi.entity.Endereco;
+import br.com.dbc.vemser.pessoaapi.entity.Pessoa;
 import br.com.dbc.vemser.pessoaapi.exceptions.RegraDeNegocioException;
 import br.com.dbc.vemser.pessoaapi.repository.EnderecoRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class EnderecoService {
 
     private final EnderecoRepository enderecoRepository;
     private final PessoaService pessoaService;
     private final ObjectMapper objectMapper;
+    private final EmailService emailService;
 
-    public EnderecoService(EnderecoRepository enderecoRepository, PessoaService pessoaService, ObjectMapper objectMapper) {
-        this.enderecoRepository = enderecoRepository;
-        this.pessoaService = pessoaService;
-        this.objectMapper = objectMapper;
-    }
 
     public List<EnderecoDTO> listarTodos() {
         return enderecoRepository.listarTodos()
@@ -49,10 +48,14 @@ public class EnderecoService {
         Endereco enderecoCriado = enderecoRepository.criarEndereco(idPessoa, enderecoEntity);
         EnderecoDTO enderecoDTO = objectMapper.convertValue(enderecoCriado, EnderecoDTO.class);
 
+        PessoaDTO dto = getPessoaDTO(enderecoCriado);
+
+        emailService.enviarEmailEnderecoCriado(enderecoDTO, dto);
+
         return enderecoDTO;
     }
 
-    public EnderecoDTO atualizarEndereco(Integer idEndereco, @Valid EnderecoCreateDTO enderecoAtualizar) throws Exception {
+    public EnderecoDTO atualizarEndereco(Integer idEndereco, EnderecoCreateDTO enderecoAtualizar) throws Exception {
         Endereco enderecoRecuperado = getEndereco(idEndereco);
 
         enderecoRecuperado.setIdEndereco(idEndereco);
@@ -64,14 +67,31 @@ public class EnderecoService {
         enderecoRecuperado.setCidade(enderecoAtualizar.getCidade());
         enderecoRecuperado.setEstado(enderecoAtualizar.getEstado());
         enderecoRecuperado.setPais(enderecoAtualizar.getPais());
+        // metodo criado pelo intelliJ
+        PessoaDTO dto = getPessoaDTO(enderecoRecuperado);
 
-        return objectMapper.convertValue(enderecoRecuperado, EnderecoDTO.class);
+        EnderecoDTO enderecoDTO = objectMapper.convertValue(enderecoRecuperado, EnderecoDTO.class);
+        emailService.enviarEmailEnderecoAtualizado(enderecoDTO, dto);
+
+        return enderecoDTO;
     }
 
 
     public void deletarEndereco(Integer idEndereco) throws Exception {
         Endereco enderecoRecuperado = getEndereco(idEndereco);
+        EnderecoDTO enderecoDTO = objectMapper.convertValue(enderecoRecuperado, EnderecoDTO.class);
+        // metodo criado pelo intelliJ
+        PessoaDTO dto = getPessoaDTO(enderecoRecuperado);
+
+        emailService.enviarEmailEnderecoDeletado(enderecoDTO, dto);
         enderecoRepository.deletarEndereco(enderecoRecuperado);
+    }
+
+    private PessoaDTO getPessoaDTO(Endereco enderecoRecuperado) throws Exception {
+        Pessoa pessoa = pessoaService.getPessoa(enderecoRecuperado.getIdPessoa());
+        PessoaDTO dto = objectMapper.convertValue(pessoa, PessoaDTO.class);
+        dto = objectMapper.convertValue(dto, PessoaDTO.class);
+        return dto;
     }
 
     // metodo para validar se o endereco existe, se não existir, lança uma exceção
