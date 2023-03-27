1e3115b19fedc1741cc01fca8c1d33c4a145c5f0
Marcklen <marcklen@icloud.com>
2023-03-27 17:34:45 -0300

    finalizando task01

--- modulo-4-1/task-1.1.js
+++ modulo-4-1/task-1.1.js
@@ -1,63 +1,87 @@
 // inserindo apenas 1
 db.alunos.insert(
-{
-    "nome" : "João",
-    "data_nascimento" : new Date (1994,02,26), 
-    "curso" : {
-        "nome" : "Sistemas de informação"
-    },
-    "notas" : [10.0, 9.0, 4.5],
-    "habilidades": [
-        {
-            "nome" : "inglês",
-            "nível" : "avançado"
+    {
+        "nome": "João",
+        "data_nascimento": new Date(1994, 02, 26),
+        "curso": {
+            "nome": "Sistemas de informação"
         },
-        {
-            "nome" : "taekwondo",
-            "nível" : "básico"
-        }
-    ]
-}
+        "notas": [10.0, 9.0, 4.5],
+        "habilidades": [
+            {
+                "nome": "inglês",
+                "nível": "avançado"
+            },
+            {
+                "nome": "taekwondo",
+                "nível": "básico"
+            }
+        ]
+    }
 )
 
 // inserindo muitos
 db.alunos.insertMany([
-{
-    "nome" : "Maria",
-    "data_nascimento" : new Date (1990,10,05), 
-    "curso" : {
-        "nome" : "Ciências da Computação"
-    },
-    "notas" : [9.0, 8.0, 5.5],
-    "habilidades": [
-        {
-            "nome" : "inglês",
-            "nível" : "básico"
+    {
+        "nome": "Maria",
+        "data_nascimento": new Date(1990, 10, 05),
+        "curso": {
+            "nome": "Ciências da Computação"
         },
-        {
-            "nome" : "beach tennis",
-            "nível" : "intermediário"
-        }
-    ]
-},
-{
-    "nome" : "Pedro",
-    "data_nascimento" : new Date (1998,01,03), 
-    "curso" : {
-        "nome" : "Redes de Telecomunicação"
+        "notas": [9.0, 8.0, 5.5],
+        "habilidades": [
+            {
+                "nome": "inglês",
+                "nível": "básico"
+            },
+            {
+                "nome": "beach tennis",
+                "nível": "intermediário"
+            }
+        ]
     },
-    "notas" : [10.0, 5.0, 6.5],
-    "habilidades": [
-        {
-            "nome" : "inglês",
-            "nível" : "avançado"
+    {
+        "nome": "Pedro",
+        "data_nascimento": new Date(1998, 01, 03),
+        "curso": {
+            "nome": "Redes de Telecomunicação"
         },
-        {
-            "nome" : "beach tennis",
-            "nível" : "básico"
-        }
-    ]
-}
+        "notas": [10.0, 5.0, 6.5],
+        "habilidades": [
+            {
+                "nome": "inglês",
+                "nível": "avançado"
+            },
+            {
+                "nome": "beach tennis",
+                "nível": "básico"
+            }
+        ]
+    }
 ])
 
 // criando as consultas personalizadas
+db.alunos.findOne(
+    { 'nome': /^P/ }
+)
+
+db.alunos.find(
+    {
+        "habilidades.nome": "beach tennis"
+    }
+).pretty()
+
+// or
+db.alunos.find({
+    $or: [
+        { "habilidades.nome": "beach tennis" },
+        { "habilidades.nome": "inglês" }
+    ]
+})
+
+// in
+db.alunos.find({
+    "curso.nome": {
+        $in: ["Sistemas de informação", "Redes de Telecomunicação"]
+    }
+})
