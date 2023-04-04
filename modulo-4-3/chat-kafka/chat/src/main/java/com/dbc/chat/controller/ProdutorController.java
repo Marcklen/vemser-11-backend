54e16cdda59512cefd1dcfe94883413dec299b7a
Marcklen <marcklen@icloud.com>
2023-04-04 20:17:55 -0300

    criado projeto inicial

---
+++ modulo-4-3/chat-kafka/chat/src/main/java/com/dbc/chat/controller/ProdutorController.java
@@ -0,0 +1,25 @@
+package com.dbc.chat.controller;
+
+//import com.dbc.chat.dto.MensagemCreateDTO;
+import com.dbc.chat.dto.NomeChat;
+import com.dbc.chat.service.ProdutorService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/kafka")
+@RequiredArgsConstructor
+public class ProdutorController {
+
+    private final ProdutorService produtorService;
+
+    @PostMapping("/send-to")
+    public void sendTo(@RequestParam List<NomeChat> chats, @RequestParam String mensagem) throws JsonProcessingException {
+        for (NomeChat nome : chats) {
+            produtorService.sendTo(mensagem, nome);
+        }
+    }
+}
---
+++ modulo-4-3/chat-kafka/chat/src/main/java/com/dbc/chat/service/ProdutorService.java
@@ -0,0 +1,10 @@
+package com.dbc.chat.service;
+
+import com.dbc.chat.dto.NomeChat;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutorService {
+    public void sendTo(String mensagem, NomeChat nome) {
+    }
+}
